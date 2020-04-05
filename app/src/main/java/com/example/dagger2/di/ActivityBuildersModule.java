53bc62e7764a7cbce1a79dc2d05a5a3450ffe124
gilles loriquer <gilles.loriquer@gfi.fr>
2020-04-05 09:19:28 +0200

    Injection dans une activité avec @ContributesAndroidInjector

---
+++ app/src/main/java/com/example/dagger2/di/ActivityBuildersModule.java
@@ -0,0 +1,15 @@
+package com.example.dagger2.di;
+
+import com.example.dagger2.AuthActivity;
+
+import dagger.Module;
+import dagger.android.ContributesAndroidInjector;
+
+@Module
+public abstract class ActivityBuildersModule {
+
+    @ContributesAndroidInjector
+    abstract AuthActivity contributeAuthActivity();
+
+
+}
